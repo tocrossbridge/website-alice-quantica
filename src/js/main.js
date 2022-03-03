984b3fb38fbf27a74371daf9b8dcc7c749f690b1
Caio Morato <morato_dev@protonmail.com>
2022-03-02 23:36:24 -0300

    feat: carousel base implementation

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,24 @@
 const navbarList = document.getElementById('navbar-list');
 
-function openNavbar(){
-    navbarList.style = "display: block";
+function openNavbar() {
+  navbarList.style = 'display: block';
 }
 
-function closeNavbar(){
-    navbarList.style = "display: none";
+function closeNavbar() {
+  navbarList.style = 'display: none';
 }
+
+$('.partners__carousel').slick({
+  centerMode: true,
+  variableWidth: true,
+  responsive: [
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+      },
+    },
+  ],
+});
