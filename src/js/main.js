526c852c071f71dc0417912115fd3bb184302e35
mitnick <beatriztattooer@gmail.com>
2022-03-02 22:22:46 -0300

    style: faq

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,13 @@
 const navbarList = document.getElementById('navbar-list');
+const collapsible = document.getElementsByClassName('collapsible');
+
+for(var i=0; i < collapsible.length; i ++){
+    collapsible[i].addEventListener('click', function(){
+        this.classList.toggle("active");
+        var content = this.nextElementSibling;
+        content = content.style.display === 'block' ? content.style.display = "None" : content.style.display = "block";
+    });
+}
 
 function openNavbar(){
     navbarList.style = "display: block";
