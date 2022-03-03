53fb4f585acb7296453f357fb4257973fa018b35
Caio Morato <morato_dev@protonmail.com>
2022-03-02 23:40:00 -0300

    fix: class styles adjusments and more options for carousel

--- src/js/main.js
+++ src/js/main.js
@@ -9,15 +9,28 @@ function closeNavbar() {
 }
 
 $('.partners__carousel').slick({
+  autoplay: true,
+  autoplaySpeed: 2000,
   centerMode: true,
   variableWidth: true,
+  slidesToScroll: 2,
+  arrows: false,
   responsive: [
+    {
+      breakpoint: 1200,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        infinite: true,
+      },
+    },
     {
       breakpoint: 600,
       settings: {
         slidesToShow: 1,
         slidesToScroll: 1,
         infinite: true,
+        autoplaySpeed: 2000,
       },
     },
   ],
