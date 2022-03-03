95a723bb284900a8b9c33e847e3a2742ab41da52
Caio Morato <morato_dev@protonmail.com>
2022-03-03 00:29:59 -0300

    feat: resources and about pages base implementation

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,15 @@
 const navbarList = document.getElementById('navbar-list');
 const collapsible = document.getElementsByClassName('collapsible');
 
-for(var i=0; i < collapsible.length; i ++){
-    collapsible[i].addEventListener('click', function(){
-        this.classList.toggle("active");
-        var content = this.nextElementSibling;
-        content = content.style.display === 'block' ? content.style.display = "None" : content.style.display = "block";
-    });
+for (var i = 0; i < collapsible.length; i++) {
+  collapsible[i].addEventListener('click', function () {
+    this.classList.toggle('active');
+    var content = this.nextElementSibling;
+    content =
+      content.style.display === 'block'
+        ? (content.style.display = 'None')
+        : (content.style.display = 'block');
+  });
 }
 
 function openNavbar() {
@@ -26,20 +29,11 @@ $('.partners__carousel').slick({
   arrows: false,
   responsive: [
     {
-      breakpoint: 1200,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 2,
-        infinite: true,
-      },
-    },
-    {
       breakpoint: 600,
       settings: {
         slidesToShow: 1,
         slidesToScroll: 1,
         infinite: true,
-        autoplaySpeed: 2000,
       },
     },
   ],
