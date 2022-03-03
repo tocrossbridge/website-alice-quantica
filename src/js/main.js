3ea2e2de1971463cfbec25b185c6e34929416741
mitnick <beatriztattooer@gmail.com>
2022-03-02 23:06:51 -0300

    perf: ternary implemented I don't know why I used 2 functions to developed this before, but now has just one with ternary

--- src/js/main.js
+++ src/js/main.js
@@ -5,14 +5,10 @@ for(var i=0; i < collapsible.length; i ++){
     collapsible[i].addEventListener('click', function(){
         this.classList.toggle("active");
         var content = this.nextElementSibling;
-        content = content.style.display === 'block' ? content.style.display = "None" : content.style.display = "block";
+        content = content.style.display === 'block' ? content.style.display = "none" : content.style.display = "block";
     });
 }
 
-function openNavbar(){
-    navbarList.style = "display: block";
-}
-
-function closeNavbar(){
-    navbarList.style = "display: none";
+function navbar(){
+    navbarList.style.display === 'block' ? navbarList.style.display = "none" : navbarList.style.display = "block"
 }
